3811d5a2ebb503d71923463c5cdaf99f76013593
surekhaachanta <achantasurekha@gmail.com>
2018-11-02 12:46:05 -0700

    removal of unwanted files

--- foodtruck/src/main/java/com/food/config/WebConfig.java
+++ foodtruck/src/main/java/com/food/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.food.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@WebAppConfiguration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
--- foodtruck/src/test/js/hello-spec.js
+++
@@ -1,6 +0,0 @@
-describe( 'hello module', function () {
-    'use strict';
-    it( 'speak()', function () {
-        expect('Hello!').toBe( 'Hello!' );
-    } );
-} );

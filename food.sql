371896b2a4b3a764248331a3e07f8047d449c1f4
surekhaachanta <36145401+surekhaachanta@users.noreply.github.com>
2018-11-21 15:16:18 -0800

    Added user details table

--- food.sql
+++ food.sql
@@ -7,23 +7,24 @@ CREATE TABLE `menu` (
   `id` int(11) NOT NULL,
   `title` varchar(75) DEFAULT NULL,
   `link` varchar(100) DEFAULT NULL,
+  `qty` int(4) Default Null,
   `price` DOUBLE(10,2) NOT NULL,
   `category` varchar(100)  NOT NULL,
   PRIMARY KEY (`id`)
 );
-
 CREATE TABLE `order_Details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_Info` varchar(200) default null,
   `total` DOUBLE(10,2) Default 0,
   `user_name` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL, 
+  `city` varchar(50) Default Null,
   `status` varchar(50) DEFAULT NULL,
+  `truck` varchar(20) DEFAULT NULL,
+   `time` timestamp,
   PRIMARY KEY (`id`)
 ) ;
-
-
 CREATE TABLE `user` (
   `user_Name` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
@@ -38,9 +39,9 @@ INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (4,'Portob
 INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (5,'Turkey','/resources/images/Food11',9, 'Burger');
 
 INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (6,'Fries','/resources/images/Food12',3.5, 'Fries');
-INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (7,'Curly fries','/resources/images/Food13',4, 'Fries');
-INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (8,'Waffle fries','/resources/images/Food13',4, 'Fries');
-INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (9,'Chili cheese fries','/resources/images/Food13',4.5, 'Fries');
+INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (7,'Curly','/resources/images/Food13',4, 'Fries');
+INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (8,'Waffle','/resources/images/Food13',4, 'Fries');
+INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (9,'Chili Cheese','/resources/images/Food13',4.5, 'Fries');
 
 INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (10,'Vegetal','/resources/images/Food13',6.5, 'Tacos');
 INSERT INTO `menu` (`id`, `title`, `link`, `price`,`category`) VALUES (11,'Carnitas','/resources/images/Food13',6.5, 'Tacos');
